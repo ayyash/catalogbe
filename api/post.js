034d3e398ec8837a2bf4f8bbeb17ada54dac70c4
ayyash <abdul_ayyash@yahoo.com>
2025-03-04 21:31:00 +0300

    first commit

---
+++ api/post.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const uuid = require('uuid').v4;
+
+module.exports = function () {
+
+    const router = express.Router();
+
+    router.get('/posts', function (req, res) {
+        const o = require('../data/posts.json');
+        if (!o) {
+            res.status(404).json({
+                code: 'NO_RESULTS',
+                message: 'No results found'
+            });
+        } else {
+            // get diary of authenticated user
+            res.json({
+                data: {
+                    total: o.length,
+                    matches: o
+                }
+            });
+
+        }
+    });
+
+
+    router.get('/posts/:id', function (req, res) {
+        const o = require('../data/post.json')
+        res.json({
+            data: o
+        });
+    });
+
+    router.get('/posts/:id/comments', function (req, res) {
+        const o = require('../data/comments.json');
+        if (!o) {
+            res.status(404).json({
+                code: 'NO_RESULTS',
+                message: 'No results found'
+            });
+        } else {
+            // get diary of authenticated user
+            res.json({
+                data: {
+                    total: o.length,
+                    matches: o
+                }
+            });
+
+        }
+    });
+    router.get('/posts/:id/comments/:cid/replies', function (req, res) {
+        const o = require('../data/replies.json');
+        if (!o) {
+            res.status(404).json({
+                code: 'NO_RESULTS',
+                message: 'No results found'
+            });
+        } else {
+            // get diary of authenticated user
+            res.json({
+                data: o
+            });
+
+        }
+    });
+    router.post('/posts/:id/comments', function (req, res) {
+        const o = require('../data/comments.json');
+        // create new by body and append
+        const c = {
+            id: uuid(),
+            content: req.body.content,
+            author: {
+                "displayname": "Johansen Deferson",
+                "avatar": "https://randomuser.me/api/portraits",
+                "shortname": "johnson"
+            },
+            created: new Date(),
+            replies: 0
+        };
+
+        o.push(c);
+
+        // get diary of authenticated user
+        res.json({
+            data: c
+        });
+
+    });
+    router.post('/posts/:id/comments/:cid/replies', function (req, res) {
+        const o = require('../data/replies.json');
+        // create new by body and append
+        const c = {
+            id: uuid(),
+            content: req.body.content,
+            author: {
+                "displayname": "Johansen Deferson",
+                "avatar": "https://randomuser.me/api/portraits",
+                "shortname": "johnson"
+            },
+            created: new Date()
+        };
+
+        o.push(c);
+
+        // get diary of authenticated user
+        res.json({
+            data: c
+        });
+
+    });
+
+    return router;
+
+}
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+// two webapis one for wen and another for admin
+
+const webapi = express();
+webapi.use(express.json());
+webapi.use(express.urlencoded({ extended: false }));
+
+webapi.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // fix
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Content-Length, Accept, Authorization, Accept-Language");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT,PATCH, DELETE, OPTIONS");
+    res.header("Cache-Control", "no-cache");
+    next();
+});
+
+// preflight for cors 
+webapi.options("/*", function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization, Content-Length, X-Requested-With, Accept, Accept-Language');
+    res.sendStatus(200);
+});
+
+
+webapi.use('/api', require('./api/post')());
+
+webapi.use('/data', express.static(__dirname + '/data'));
+
+webapi.use('/api/*', function (req, res, next) {
+    // throw 404   
+    res.status(404);
+    const err = new Error('404');
+    err['status'] = 404;
+    next(err);
+});
+
+webapi.use(function (err, req, res, next) {
+
+    console.log(err.stack);
+    res.status(err.status || 500).json({
+        message: err.message,
+        code: err.code || 'UNKNOWN'
+    });
+});
+const port = process.env.PORT || 1300;
+
+webapi.listen(port, function (err) {
+    console.log('listning to ' + port);
+
+    if (err) {
+        console.log(err);
+        return;
+    }
+});
+
